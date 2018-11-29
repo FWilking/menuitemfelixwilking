31daf45ecdad9e4e5c14c0a5377267f1330fcf36
BT_1N3_16 <fwilking4736@bths.edu>
2018-11-29 08:18:01 -0500

    Initial commit

---
+++ src/felixwilking/trio.java
@@ -0,0 +1,13 @@
+package felixwilking;
+
+public class trio implements menuitem {
+    private final String sandwich;
+    private final String salad;
+    private final String drink;
+
+    public trio(String sandwich, String salad, String drink){
+        this.sandwich = sandwich;
+        this.salad = salad;
+        this.drink = drink;
+    }
+}
